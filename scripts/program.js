7eb1c2603e9f99c1f2c27fd6788beefa4425f889
chkap <chkhappy@hotmail.com>
2017-06-25 18:58:45 +0800

    ...

--- scripts/program.js
+++ scripts/program.js
@@ -5,23 +5,47 @@
 
 define([], function () {
 
-    class BaseProgram{
-        static _prog = null;
+
+
+    class ProgramManager{
 
         constructor(gl){
             this._gl = gl;
-            this._vertex_source = `
-                attribute vec4 a_position;
-                void main(){
-                    gl_Position = a_position;
-                }
-                `;
-            this._fragment_source = `
-                void main(){
-                    gl_FragColor = vec4(1,0,0,1);
-                }
-                `;
+            this._program_map = new Map();
+        }
 
+        _createProgram(prog_class){
+            if (Object.getPrototypeOf(prog_class) !== _BaseProgram){
+                alert('cannot create a program not defined.');
+                return;
+            }
+            if(this._program_map.has(prog_class.programName))
+                return;
+            let program_instance = new prog_class();
+            this._program_map.set(prog_class.programName, program_instance)
+        }
+
+        selectProgram(prog_class){
+            if (Object.getPrototypeOf(prog_class) !== _BaseProgram){
+                alert('cannot create a program not defined.');
+                return;
+            }
+            if (!this._program_map.has(prog_class.programName)){
+                this._createProgram(prog_class)
+            }
+            this._program_map.get(prog_class.programName).enable();
+        }
+    }
+
+    class _BaseProgram{
+        constructor(gl, vertex_source, fragment_source){
+            this._gl = gl;
+            this._vertex_source = vertex_source;
+            this._fragment_source = fragment_source;
+            this._compileProgram();
+        }
+
+        _compileProgram(){
             this._vertex_shader = gl.createShader(gl.VERTEX_SHADER);
             gl.compileShader(this._vertex_shader, this._vertex_source);
             if (!gl.getShaderParameter(this._vertex_shader, gl.COMPILE_STATUS)) {
@@ -42,28 +66,51 @@ define([], function () {
             gl.linkProgram(this._program);
             if (!gl.getProgramParameter(this._program, gl.LINK_STATUS)){
                 alert("Could not initialise shaders");
-                return;
+                return ;
             }
+        }
 
+        enable(){
+            alert('You cannot enable the _BaseProgram. Enable a subclass program of it.');
+        }
+
+        static get programName(){ return '_BaseProgram';}
+
+    }
+
+    class PosProgram extends _BaseProgram{
+
+        constructor(gl){
+            vertex_source = `
+                attribute vec4 a_position;
+                void main(){
+                    gl_Position = a_position;
+                }
+                `;
+            fragment_source = `
+                void main(){
+                    gl_FragColor = vec4(1,0,0,1);
+                }
+                `;
+            super(gl, vertex_source, fragment_source);
         }
 
         enable(){
             if (this._gl && this._program){
                 this._gl.useProgram(this._program);
+            }else{
+                alert('The program is not valid.')
             }
         }
 
-        static create(){
-            if(!BaseProgram._prog){
-                BaseProgram._prog = new BaseProgram()
-            }
-        }
+        static get programName(){ return 'PosProgram'}
     }
 
     return {
+        ProgramManager,
         BaseProgram,
     }
 
 
-})
+});
 
